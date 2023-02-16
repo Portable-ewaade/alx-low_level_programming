5ff4aae36916a11e514a356407d30bbfe489e3f1
Portable-ewaade <portableewaade.com>
2023-02-16 15:49:26 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**main - A program thaat prints a line to the standard error
- *Return: Always 1 (Success)
-*/
+ *Return: Always 1 (Success) */
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
