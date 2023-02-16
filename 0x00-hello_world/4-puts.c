0f2dafe8b4e76dbfbe06dc76dc9d25a7df78bbfa
Portable-ewaade <portableewaade.com>
2023-02-16 14:24:50 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
- * Return: 0 (Success)
+ *main - A program that prints a line with puts function
+ *Return: 0 (Success)
 */
 int main(void)
 {
-put("\"Programming is like building multilingual puzzle")
+put("\"Programming is like building multilingual puzzle");
 return (0);
 }
