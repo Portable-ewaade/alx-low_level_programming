6d9a169d25f6d79ad7dbfaabea83f80e40efaa2e
Portable-ewaade <portableewaade.com>
2023-02-21 12:33:54 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * print_alphabet - a function that prints the alphabet, in lowercase
- *
- * Return: Always 0 (Succes)
+ * print_alphabet - prints all alphabets in lowercase.
+ * Return: On success 1.
  */
 void print_alphabet(void)
 {
-	int n;
+	char i;
 
-	for (n = 'a'; n <= 'z'; n++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
-		_putchar(n);
+	_putchar(i);
 	}
 	_putchar('\n');
+
 }
+
