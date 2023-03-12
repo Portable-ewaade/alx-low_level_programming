d4bdceacdbf1caa341e73035a759c5452c65ab36
Portable-ewaade <portableewaade.com>
2023-03-12 11:23:59 -0700

    grroup commit

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
- * _isupper - uppercase letters
- * @c: char to check
- *
- * Return: 0 or 1
+ * _isupper - program starts
+ * @c: integer input
+ * program checks alphabets for uppercase.
+ * Return: 1 if upper and 0 is not
  */
+
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
+	{
 		return (1);
+	}
 	else
-	return (0);
+		return (0);
 }
-
